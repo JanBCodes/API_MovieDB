f18f2e27c7e2e70a57e98ca58332d440c8320cb1
JanBCodes <janiellebacchus@gmail.com>
2020-06-26 05:12:23 -0400

    Started working on the JavaScript

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 function init()
 {
-    const displayMenu =document.querySelector("span")
+    let click ="ON"
+    const displayMenu =document.querySelector("#hamburgerMeanIcon")
     displayMenu.addEventListener(`click`, ()=> {
 
-        console.log(`Clicked`)
-        
+        if(click=="ON")
+        {
+            const menu=document.querySelector("#Menu")
+            menu.style.display=`block`  
+            
+            click="OFF"
+        }
+        else
+        {
+            const menu=document.querySelector("#Menu")
+            menu.style.display=`none`  
+            
+            click="ON"
+        }
+
+
 
     })
 }
@@ -14,21 +29,26 @@ init()
 function api()
 {
 
-        const endPoint=`https://api.themoviedb.org/3/movie/550?api_key=1b562e1d6f5dd3a38fcbe32f5c01f875`
-        console.log(endPoint)
+        const endPoint=`https://api.themoviedb.org/3/movie/now_playing?api_key=1b562e1d6f5dd3a38fcbe32f5c01f875&language=en-US&page=1`
+        const latestMovie=`https://api.themoviedb.org/3/movie/latest?api_key=1b562e1d6f5dd3a38fcbe32f5c01f875&language=en-US`
+        
+        console.log(latestMovie)
 
         //This code is used to get a response from the REST API.
-        fetch(endPoint) //asynchronous 
+        fetch(latestMovie) //asynchronous 
 
         .then(function(response){
 
             response.json() // async operation 
             .then(function(data){
 
-            /*const div = document.querySelector("#displayWeather");
-            div.innerHTML=`The current temperature in ${dropDownList.value} is ${data.current.temperature}`;
+            const movieDiv = document.querySelector(".movieContainer");
+            //movieDiv.innerHTML="" THIS IS `${data.results[0].title}`"
+            
+            
+            /*`The current temperature in ${dropDownList.value} is ${data.current.temperature}`;
             div.innerHTML+=`<br>The current weather description is ${data.current.weather_descriptions[0]} <br>`;
-        
+                /*
             //Dynamically created elements
             const weatherImg = document.createElement("img");
             weatherImg.setAttribute("src",data.current.weather_icons[0]);
