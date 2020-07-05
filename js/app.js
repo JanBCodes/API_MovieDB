7d37266e42bb3664e100b0f0f3963af7f9a6a8da
JanBCodes <janiellebacchus@gmail.com>
2020-07-05 07:01:27 -0400

    First Page-Completed API Pull

--- js/app.js
+++ js/app.js
@@ -1,67 +1,66 @@
-function init()
-{
-    let click ="ON"
-    const displayMenu =document.querySelector("#hamburgerMeanIcon")
-    displayMenu.addEventListener(`click`, ()=> {
-
-        if(click=="ON")
-        {
-            const menu=document.querySelector("#Menu")
-            menu.style.display=`block`  
-            
-            click="OFF"
-        }
-        else
-        {
-            const menu=document.querySelector("#Menu")
-            menu.style.display=`none`  
-            
-            click="ON"
-        }
-
-
-
-    })
-}
-init()
-
-
-function api()
+const api = () =>
 {
 
-        const endPoint=`https://api.themoviedb.org/3/movie/now_playing?api_key=1b562e1d6f5dd3a38fcbe32f5c01f875&language=en-US&page=1`
-        const latestMovie=`https://api.themoviedb.org/3/movie/latest?api_key=1b562e1d6f5dd3a38fcbe32f5c01f875&language=en-US`
+    //const apilatestMovie=`https://api.themoviedb.org/3/movie/latest?api_key=1b562e1d6f5dd3a38fcbe32f5c01f875&language=en-US`
+    //const apilatestMovie=`https://api.themoviedb.org/3/movie/latest?api_key=1b562e1d6f5dd3a38fcbe32f5c01f875&language=en-US`
+
+    
+    const apiEndPoint=`https://api.themoviedb.org/3/movie/now_playing?api_key=1b562e1d6f5dd3a38fcbe32f5c01f875&language=en-US&page=`
+    let endPointPage=1;
+    const endPoint=`${apiEndPoint}${endPointPage}`
+
+    fetch(endPoint) 
+
+    .then(function(response){
+
+        response.json() 
+        .then(function(data){
+
+        const movieDiv = document.querySelectorAll(".movieContainer");
+        let width = 500;
         
-        console.log(latestMovie)
+       for( i= 0; i < data.results.length ; i++)
+        {
+            movieDiv[i].innerHTML=`<h2 id="h2"> ${data.results[i].title} <h2>`
+            movieDiv[i].innerHTML+=`<h3 id="voteAvg"> Vote Average:${data.results[i].vote_average} <h3>`
+            movieDiv[i].innerHTML+=`<img src="https://image.tmdb.org/t/p/original/${data.results[i].poster_path}" width="${width}px">`
+            movieDiv[i].innerHTML+=`<p> ${data.results[i].overview} <p>`
+            movieDiv[i].innerHTML+=`<p> Release Date: ${data.results[i].release_date} <p>`
 
-        //This code is used to get a response from the REST API.
-        fetch(latestMovie) //asynchronous 
+        }
 
-        .then(function(response){
-
-            response.json() // async operation 
-            .then(function(data){
-
-            const movieDiv = document.querySelector(".movieContainer");
-            //movieDiv.innerHTML="" THIS IS `${data.results[0].title}`"
-            
-            
-            /*`The current temperature in ${dropDownList.value} is ${data.current.temperature}`;
-            div.innerHTML+=`<br>The current weather description is ${data.current.weather_descriptions[0]} <br>`;
-                /*
-            //Dynamically created elements
-            const weatherImg = document.createElement("img");
-            weatherImg.setAttribute("src",data.current.weather_icons[0]);
-            div.appendChild(weatherImg);// must append!
-                */
-            })
-            .catch(function(err){
-                console.log(`Error :${err}`)
-            })
         })
         .catch(function(err){
             console.log(`Error :${err}`)
         })
+    })
+    .catch(function(err){
+        console.log(`Error :${err}`)
+    })
 
+};
+api()
+
+const app = () => {
+
+    window.addEventListener('DOMContentLoaded', (event) => {
+        console.log('DOM fully loaded and parsed');
+        
+
+        const main = () => {
+
+        
+    
+        }
+        main()
+
+
+
+
+
+    });
+
+   
 }
-api();
+app()
+
